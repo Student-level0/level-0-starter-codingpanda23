1149aa35a8a25b8dd794a486cda424fe00953816
Leagueathyst <league@WTS5.attlocal.net>
2017-07-22 15:58:43 -0700

    7-22-17

---
+++ src/Package/ForLoopChallenges.java
@@ -0,0 +1,106 @@
+/*CHALLENGE 1
+ 
+package Package;
+
+public class ForLoopChallenges {
+public static void main(String[] args) {
+	for (int i = 0; i < 101; i++) {
+	System.out.println(i);	
+	}
+}
+}
+*/
+/*CHALLENGE 2
+ 
+ 
+package Package;
+ 
+
+public class ForLoopChallenges {
+public static void main(String[] args) {
+	for (int i = 100; i > 0; i--) {
+	System.out.println(i);	
+	}
+}
+}
+*/
+/*CHALLENGE 3
+ 
+ 
+package Package;
+ 
+
+public class ForLoopChallenges {
+public static void main(String[] args) {
+	for (int i = 2; i < 101; i=i+2) {
+	System.out.println(i);	
+	}
+}
+}
+*/
+/*CHALLENGE 4
+ 
+ 
+package Package;
+ 
+
+public class ForLoopChallenges {
+public static void main(String[] args) {
+	for (int i = 1; i < 100; i=i+2) {
+	System.out.println(i);	
+	}
+}
+}
+*/
+/*CHALLENGE 5
+ 
+ 
+package Package;
+ 
+
+public class ForLoopChallenges {
+public static void main(String[] args) {
+	for (int i = 1; i < 501; i++) {
+	if (i%2==0) {
+		System.out.println(i+" even");
+	}
+	if (i%2==1) {
+		System.out.println(i+" odd");
+	}	
+	
+	}
+}
+}
+*/
+/*CHALLENGE 6
+ 
+ 
+package Package;
+ 
+
+public class ForLoopChallenges {
+public static void main(String[] args) {
+	for (int i = 0; i < 778; i=i+7) {
+	System.out.println(i);	
+	}
+}
+}
+*/
+/*CHALLENGE 7
+ 
+ 
+package Package;
+
+import javax.swing.JOptionPane;
+
+public class ForLoopChallenges {
+public static void main(String[] args) {
+	String answer = JOptionPane.showInputDialog("How old are you?");
+	int age = Integer.parseInt(answer);
+	for (int i = 2017 - age; i <= 2017; i++) {
+		System.out.println(i+", I was "+(i-(2017-age)));
+	}	
+	}
+
+}
+*/
